8a3dfd009c876121f506065fbd9b1ede4a34554e
Corinne Schlachter <corinne@corinnes-mbp.lan>
2021-01-29 14:58:06 -0500

    fixed bug in count inversions which was not working with np.arrays. Now accepts list types

--- CountInv.py
+++ CountInv.py
@@ -1,22 +1,53 @@
 import pytest
+import numpy as np
 
 
 def count_inversions(x):
     """
     Function counts the number of inversions, which is a pair of elements that are out of order, in an array x.
-    INPUT: x array
+    INPUT: x list
     OUTPUT: Number of inversions in x integer
     """
-    count = 0
+
     n = len(x)
 
-    # base case
-    if n <= 1:
-        return 0
+    if n == 1:
+        # base case
+        return x, 0
     else:
-        x_left = x[range(0, n/2)]
-        x_right = x[range(n/2, n)]
+        x_left = x[:n // 2]
+        x_right = x[n // 2:]
 
-        split_inversions = count_inversions(x)
+        x_left, x_left_i = count_inversions(x_left)
+        x_right, x_right_i = count_inversions(x_right)
+        c = []
 
-    return count
+        i = 0
+        j = 0
+        inversions = 0 + x_left_i + x_right_i
+
+        while i < len(x_left) and j < len(x_right):
+            if x_left[i] < x_right[j]:
+                c.append(x_left[i])
+                i += 1
+            else:
+                c.append(x_right[j])
+                j += 1
+                inversions += (len(x_left) - i)
+
+        c += x_left[i:]
+        c += x_right[j:]
+
+    return c, inversions
+
+
+def test_small_array(x=[0, 1, 2, 3]):
+    out_array, inversions = count_inversions(x)
+    assert inversions == 0
+
+
+def test_odd_len_array(x=[0, 7, 2, 3]):
+    out_array, inversions = count_inversions(x)
+    assert inversions == 2
+
+
